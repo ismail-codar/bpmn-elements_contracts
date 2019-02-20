2694ffefdf9f6ae238308c422a162a2eed6a5c63
Vyperus <bastian.schnittger@5minds.de>
2019-02-20 16:38:18 +0100

    :sparkles: A Proess Parent Is a Definition

--- src/bpmnElements/IProcessRef.ts
+++ src/bpmnElements/IProcessRef.ts
@@ -1,6 +1,8 @@
+import {IDefinitions} from './IDefinitions';
 import {IModdleElement} from './IModdleElement';
 
 export interface IProcessRef extends IModdleElement {
+  $parent: IDefinitions;
   extensionElement: IModdleElement;
   flowElement: Array<IModdleElement>;
   isExecutable: boolean;
